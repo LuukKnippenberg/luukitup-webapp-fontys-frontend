37f9e5741a15a337ef9e3d28d6e84faa27d57100
Luuk Knippenberg <luuk_knippenberg@hotmail.com>
2021-06-02 22:48:04 +0200

    Fixed some routing

--- luukitup/src/router/index.js
+++ luukitup/src/router/index.js
@@ -19,8 +19,8 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/Projects',
-    name: 'Projects',
+    path: '/Projecten',
+    name: 'Projecten',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -57,6 +57,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/CMS/Blog.vue')
+  },
+  {
+    path: '/CMS/Projecten',
+    name: 'Projects CMS',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/CMS/Projects.vue')
   }
 
 ]
